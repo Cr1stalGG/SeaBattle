14065d631455bf510b922eb2f5a89ff16d1c6f02
Alexander_Savickij <alexandersavickij2@gmail.com>
2024-01-28 04:12:43 +0300

    Fill LetterToIntConvertor with methods

--- src/main/java/by/seabattle/utils/LetterToIntConvertor.java
+++ src/main/java/by/seabattle/utils/LetterToIntConvertor.java
@@ -1,5 +1,20 @@
 package by.seabattle.utils;
 
-public class LetterToIntConvertor {
+import lombok.experimental.UtilityClass;
 
+@UtilityClass
+public class LetterToIntConvertor {
+	private static final String letters = "ABCDEFGHIJKLMNOP";
+	
+	public static int convertLetterToInt(char letter) {
+		for (int i = 0; i < 16; ++i) 
+			if(letters.charAt(i) == letter)
+				return i;
+		
+		return -1;
+	}
+	
+	public static char convertIntToLetter(int pos) {
+		return letters.charAt(pos-1);
+	}
 }
